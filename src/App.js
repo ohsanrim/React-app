78cbd49287533bab5d151f4c427c099ede3510c8
OHrin <ka28@naver.com>
2020-06-29 16:46:52 +0900

    [React] 리액트 연습 파일

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Subject extends Component{
+  render(){
+    return(
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+       </header>
+    );
+  }
+}
+class TOC extends Component{
+  render(){
+    return(
+      <nav>
+    <ul>
+        <li><a href="1.html">HTML</a></li>
+        <li><a href="2.html">CSS</a></li>
+        <li><a href="3.html">JAVASCRIPT</a></li>
+    </ul>
+</nav>
+
+    );
+  }
+}
+class Content extends Component{
+  render(){
+    return(
+      <article>
+        <h2>HTML</h2>
+          html is HyperText Markup Language.
+      </article>
+
+    );
+  }
+}
+
+class App extends Component{
+  constructor(props){
+    super(props){
+      this.state={
+        subject:{title:'web',sub:'world wid web!'}
+      }
+    }
+  }
+  render(){
+    return(
+      <div className="App">
+       <subject title={this.state.subject.title} sub={this.state.subject.sub}></subject>
+       <TOC></TOC>
+       <Content></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
